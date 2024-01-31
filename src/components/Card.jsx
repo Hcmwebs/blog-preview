85420e26e4c79d1fd8401b34ae46fdf3d4259fc4
Heze <64218603+Hcmwebs@users.noreply.github.com>
2024-01-31 15:07:01 +1300

    modified the box shadow for large screens

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ import { CardHeader, CardBody, CardFooter } from '../components';
 const Card = () => {
 	return (
 		<main className='mx-auto grid place-items-center min-h-screen'>
-			<div className='card w-[327px] lg:w-full max-w-96 bg-secondary shadow-[8px_8px_5px_0px_hsl(0,0%,7%)] rounded-[20px] border-[1px] border-black gap-y-6 p-6 items-start'>
+			<div className='card w-[327px] lg:w-full max-w-96 bg-secondary shadow-[8px_8px_0px_0px_hsl(0,0%,7%)] lg:shadow-[16px_16px_0px_0px_hsl(0,0%,7%)] rounded-[20px] border-[1px] border-black gap-y-6 p-6 items-start'>
 				<CardHeader />
 				<CardBody />
 				<CardFooter />
